431219a5f36c36da6e8a09dd824aebb3df5451a5
Nicolas Dupont <npg.dupont@gmail.com>
2022-12-06 15:19:37 +0100

    Replace document types by terms types

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import { readFile } from 'fs/promises';
 
-export default JSON.parse(await readFile(new URL('./documentTypes.json', import.meta.url)));
+export default JSON.parse(await readFile(new URL('./termsTypes.json', import.meta.url)));
